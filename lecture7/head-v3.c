c5f1573f36565ac1160761c9c813eff5abf8a78c
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-07-25 20:42:00 +0900

    make head command

--- lecture7/head-v3.c
+++ lecture7/head-v3.c
@@ -1,27 +1,51 @@
 /*
-処理対象は標準入力のみ
+処理対象は標準入力とファイルからの読み込み
 表示したい行数をコマンドライン引数から指定する
-ファイル名をコマンドライン引数から受け取れるようにする
+
+この実装では、オプション解析をメインに行う
+静的に解析しても構わないが、それ用の API を用いて実装していく
 
 使い方
-$ ./head-v2.out 10 text
+$ ./head-v3.out -n15 text
+$ cat text | ./head-v3.out -n15
+$ ./head-v3.out -h
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
+#define DEFAULT_N_LINES 10
+
 static void do_head(FILE *f, long nlines);
 
+static struct option long_options[] = {
+  {"lines", required_argument, NULL, 'n'},
+  {"help", no_argument, NULL, 'h'},
+  {0, 0, 0, 0}
+};
+
 int main(int argc, char *argv[]) {
-  // 行数が指定されていない時は、エラーを吐く
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
-    exit(1);
+  int opt;
+  long nlines = DEFAULT_N_LINES;
+  
+  while ((opt =  getopt_long(argc, argv, "n:", long_options, NULL)) != -1) {
+    switch(opt) {
+      case 'n':
+        nlines = atol(optarg);
+        break;
+      case 'h':
+        fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+        exit(0);
+      case '?':
+        fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+        exit(1);
+    }
   }
 
-  long nlines = atol(argv[1]);
-
-  if (argc == 2) {
+  if (optind == argc) {
     do_head(stdin, nlines);
   } else {
     int i;
