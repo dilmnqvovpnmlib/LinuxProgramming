0055f54499c4565190b6d1aaad57bf4f1f3ab4dd
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-07-27 19:22:00 +0900

    make a command

---
+++ lecture10/chmod.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+  printf("Start \n");
+
+  if (argc < 2) {
+    fprintf(stderr, "%s: no arguments \n", argv[0]);
+    exit(1);
+  }
+
+  int mode;
+
+  mode = strtol(argv[1], NULL, 8);
+  for (int i = 2; i < argc; i++) {
+    if (chmod(argv[i], mode) < 0) {
+      perror(argv[i]);
+    }
+  }
+  exit(0);
+}
